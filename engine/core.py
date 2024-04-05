5dd16298804460bc9749dac04054a34cafb548aa
Xellu <83588955+xellu@users.noreply.github.com>
2024-04-05 15:52:59 +0700

    updated db indentation

--- engine/core.py
+++ engine/core.py
@@ -15,8 +15,6 @@ class Version:
 
 Config = config.Config("data/config.json")
 
-
-
 #Update logs ----------------
 threading.Thread(target=console.logging.auto_update).start()
 
@@ -29,13 +27,13 @@ class AutosaveManager:
         self.tag = console.tag(console.COLOR.MAGENTA, "Autosave")
         
         self.thread = threading.Thread(target=self._autosave)
-        # self.thread.start()
+        self.thread.start()
         
     def _autosave(self):
         time.sleep(5)
         while self.running:
             for db in self.dbs:
-                db.save(indent = Config.localhost)
+                db.save(indent = True)
             self.tag.print(f"Saved {len(self.dbs)} databases")
             
             for _ in range(self.interval):
@@ -46,13 +44,13 @@ class AutosaveManager:
     def stop(self):
         self.tag.print("Waiting for service to shutdown")
         self.running = False
-        # self.thread.join()
+        self.thread.join()
         
         self.tag.print("Saving data")
         for db in self.dbs:
-            db.save(indent = Config.localhost)
+            db.save(indent = True)
             
 Autosave = AutosaveManager(
     dbs = [
 
-    ]) #disabled -> not used for now
+    ])
